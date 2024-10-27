4f6bbd6c6c9268300f34849ed3de4fd6a72feec3
Jacob Angelo De Villa <71298648+jcdevilla01@users.noreply.github.com>
2024-10-27 08:12:25 +0000

    Updated Views.py to include login requirement to access the home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -8,3 +10,6 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
+@login_required
+def home_view(request):
+    return render(request, 'home.html')
