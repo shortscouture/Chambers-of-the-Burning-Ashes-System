7741f6993039fed99748bc12f36985be890d9b65
Kyle Santos <kpsantos@student.apc.edu.ph>
2025-01-08 16:07:05 +0800

    Change: rename to existing db

--- django_project/settings/local.py
+++ django_project/settings/local.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'testdb.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Local static and media file settings
