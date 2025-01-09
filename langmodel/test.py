8ede96cb99da9249acb6c4bed5a67bfac485e28d
Kyle Santos <kpsantos@student.apc.edu.ph>
2025-01-10 00:13:44 +0800

    Updated requirements.text, test,py has rate limit issues

--- langmodel/test.py
+++ langmodel/test.py
@@ -1,31 +1,26 @@
 from openai import OpenAI
 import environ
 import os
-
+from pathlib import Path
+from tenacity import (
+    retry, stop_after_attempt, wait_random_exponential,
+)
 
 env = environ.Env(
     DEBUG=(bool, False) #default value for DEBUG = False
 )
 
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Read env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 #secret key
-OPENAI_KEY = env("OPENAI_SECRET_KEY")
+OPENAI_KEY = env("OPENAI_API_KEY")
 client = OpenAI()
 
 
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
+@retry(wait=wait_random_exponential(min=1, max=30), stop=stop_after_attempt(2))
+def completion_with_backoff(**kwargs):
+  return client.completions.create(**kwargs)
 
-print(completion.choices[0].message)
-
+completion_with_backoff(model="gpt-3.5-turbo", prompt="Once upon a time,", max_tokens=20)
