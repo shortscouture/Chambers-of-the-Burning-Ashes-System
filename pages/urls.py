93c5c7b221faa20a253e90d714e10fc31a2b9f5e
Janson Pagharion <jhpagharion2@student.apc.edu.ph>
2025-01-09 11:24:43 +0800

    Fixed error

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, maindashview, columbaryrecordsview, customerhomeview, ColumbaryRecordListView
+from .views import HomePageView, AboutPageView, maindashview, columbaryrecordsview, customerhomeview
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.views.generic import ListView  # Import ListView
+
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
@@ -11,3 +13,9 @@ class AboutPageView(TemplateView):
     
 class maindashview(TemplateView):
     template_name = "pages/maindash.html"
+
+class customerhomeview(TemplateView):
+    template_name = "pages/Customer_Home.html"
+
+class columbaryrecordsview(TemplateView):
+    template_name = "pages/columbaryrecords.html"
