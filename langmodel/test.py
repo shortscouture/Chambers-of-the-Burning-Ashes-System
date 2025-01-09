afedd0b59692566e8dd6a60f8f43a9eff1a15ddb
Kyle Santos <kpsantos@student.apc.edu.ph>
2025-01-10 00:37:16 +0800

    Added code that checks how much tokens were used, depending on rate limits

--- langmodel/test.py
+++ langmodel/test.py
@@ -16,6 +16,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 #secret key
 OPENAI_KEY = env("OPENAI_API_KEY")
+
+
 client = OpenAI()
 
 
@@ -24,3 +26,11 @@ def completion_with_backoff(**kwargs):
   return client.completions.create(**kwargs)
 
 completion_with_backoff(model="gpt-3.5-turbo", prompt="Once upon a time,", max_tokens=20)
+
+response = client.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "What's the weather?"}]
+)
+
+
+print(response.headers)
