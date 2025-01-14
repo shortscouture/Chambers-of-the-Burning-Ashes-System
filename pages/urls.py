d46b8e481b14ef583c93e9029402855952894550
David Naldo <dcnaldo@apc.edu.ph>
2025-01-14 12:14:30 +0800

    a

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView, maindashview, columbaryrecordsview, customerhomeview, memorialview
+from .views import HomePageView, AboutPageView, maindashview, columbaryrecordsview, customerhomeview, memorialview, send_letter_of_intent
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("columbaryrecords/", columbaryrecordsview.as_view(), name="columbaryrecords"),
     path("Customer_Home/", customerhomeview.as_view(), name="Customer_Home"),
     path("Memorials/", memorialview.as_view(), name="Memorials"),
+    path('letter/', send_letter_of_intent, name='letter'),
 ]
