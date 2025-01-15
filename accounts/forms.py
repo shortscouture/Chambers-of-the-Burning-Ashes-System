dd6878254731b0a37568c5672653590dc0eeabc9
Kyle Santos <kpsantos@student.apc.edu.ph>
2025-01-15 23:14:41 +0800

    added SignUpForm class and imported Captcha module.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from captcha.fields import CaptchaField
 
 class CustomUserCreationForm(UserCreationForm):
 
@@ -13,3 +14,10 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'username',)
+        
+class SignUpForm(UserCreationForm):
+    c = CaptchaField()
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'captcha') #new and confirm password for password 1 and 2
