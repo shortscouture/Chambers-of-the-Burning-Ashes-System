d2c7fa8516ca60d74ce655a7025d763d4ffcf636
kpsantos <kpsantos@student.apc.edu.ph>
2025-02-07 09:15:07 +0800

    Added views.py

---
+++ django_project/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai.api_key = 'YOUR_API_KEY'
+
+def get_completion(prompt):
+    print(prompt)
+    query = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    response = query.choices[0].text
+    print(response)
+    return response
+
+
+def query_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        response = get_completion(prompt)
+        return JsonResponse({'response': response})
+    return render(request, 'index.html')
