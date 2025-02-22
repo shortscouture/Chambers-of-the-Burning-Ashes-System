b7895e6d0e1c19933310a8ce294d6c8240686dd4
kpsantos <kpsantos@student.apc.edu.ph>
2025-02-22 22:59:42 +0800

    Conflict with apps.py due to different name fixed

--- parish_staff/apps.py
+++ parish_staff/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ParishChatbotConfig(AppConfig):
+class ParishStaffConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parish_chatbot'
+    name = 'parish_staff'
