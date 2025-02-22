cdc3dc30bb057ba4f68f9479e38ab12cf877ec3c
kpsantos <kpsantos@student.apc.edu.ph>
2025-02-22 22:22:40 +0800

    Changed the models name to recognize CustomUser

--- parish_staff/models.py
+++ parish_staff/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from accounts.models import CustomUser
 # Create your models here.
+class ChatLog(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Links to Django users
+    message = models.TextField()  # User's message
+    response = models.TextField()  # Bot's response
+    timestamp = models.DateTimeField(auto_now_add=True)  # Auto timestamp
+
+    def __str__(self):
+        return f"Chat by {self.user.username} at {self.timestamp}"
