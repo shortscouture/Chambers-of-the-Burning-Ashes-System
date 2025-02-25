ef9f13fea135291822a3de0452e73112952cfc80
jcdevilla01 <jcdevilla@student.apc.edu.ph>
2025-02-26 00:11:42 +0800

    fixed urls redundancy

--- pages/urls.py
+++ pages/urls.py
@@ -6,9 +6,8 @@ from .views import (
     CustomerHomeView, MemorialView, send_letter_of_intent, verify_otp,
     memorials_verification, accept_letter_of_intent, decline_letter_of_intent,
     RecordsDetailsView, CustomerEditView, SuccesView, ChatbotAPIView, DashboardView, get_crypt_status, MapView, CustomerDeleteView,
-    process_ocr,get_vault_data
+    get_vault_data, upload_and_process
 )
-
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
@@ -28,13 +27,10 @@ urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
     path('get_crypt_status/<str:section>/', get_crypt_status, name='get_crypt_status'),
-    path("Columbary_Map/    ", MapView.as_view(), name="Columbary_Map"),    
-    path('columbaryrecords/', views.ColumbaryRecord, name='columbaryrecords'),
+    path("Columbary_Map/", MapView.as_view(), name="Columbary_Map"),
     path('addnewrecord/', views.addnewrecord, name='addnewrecord'),
     path('delete_customer/<int:customer_id>/', CustomerDeleteView.as_view(), name='delete_customer'),
-    path('process-ocr/', views.process_ocr, name='process_ocr'),
     path('get_vault_data/<str:section_id>/', get_vault_data, name='get_vault_data'),
-  
+    path('upload_and_process/', views.upload_and_process, name='upload_and_process'),  
     path('addnewcustomer/', views.addnewcustomer, name='addnewcustomer'),
-
 ]
