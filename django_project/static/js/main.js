ce4b5a815d681bfba217da340d7520131f0aa2f6
David Naldo <dcnaldo@student.apc.edu.ph>
2025-03-06 13:52:07 +0800

    Had some big changes in JS

--- django_project/static/js/main.js
+++ django_project/static/js/main.js
@@ -1,48 +1,78 @@
-/* 
-
-Vanilla Template
-
-https://templatemo.com/tm-526-vanilla
-
-*/
-
 jQuery(document).ready(function($) {
 
 	'use strict';
 
-    var top_header = $('.parallax-content');
-    top_header.css({'background-position':'center center'}); // better use CSS
 
-    $(window).scroll(function () {
-    var st = $(this).scrollTop();
-    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
-    });
+        $('.counter').each(function() {
+          var $this = $(this),
+              countTo = $this.attr('data-count');
+          
+          $({ countNum: $this.text()}).animate({
+            countNum: countTo
+          },
 
+          {
 
-    $('body').scrollspy({ 
-        target: '.fixed-side-navbar',
-        offset: 200
-    });
-      
-      // smoothscroll on sidenav click
+            duration: 8000,
+            easing:'linear',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
 
-    var owl = $("#owl-testimonials");
-
-      owl.owlCarousel({
-        
-        pagination : true,
-        paginationNumbers: false,
-        autoPlay: 6000, //Set AutoPlay to 3 seconds
-        items : 3, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-        
-    });
+          });  
+          
+        });
 
 
 
+        $(".b1").click(function () {
+            $(".pop").fadeIn(300);
+            
+        });
+		
+		$(".b2").click(function () {
+            $(".pop2").fadeIn(300);
+            
+        });
+		
+		$(".b3").click(function () {
+            $(".pop3").fadeIn(300);
+            
+        });
+
+        $(".pop > span, .pop").click(function () {
+            $(".pop").fadeOut(300);
+        });
+		
+		$(".pop2 > span, .pop2").click(function () {
+            $(".pop2").fadeOut(300);
+        });
+		
+		$(".pop3 > span, .pop3").click(function () {
+            $(".pop3").fadeOut(300);
+        });
+
+
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".header").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+               $(".header").removeClass("active");
+            }
+        });
+
+
+	/************** Mixitup (Filter Projects) *********************/
+    	$('.projects-holder').mixitup({
+            effects: ['fade','grayscale'],
+            easing: 'snap',
+            transitionSpeed: 400
+        });
 
 });
 
